563eda4bca8903faf76c663a177963adc6d03da2
IamRiyudo <yuridourado1900@gmail.com>
2025-04-07 11:19:57 -0300

    Primeiro projeto angular

---
+++ src/app/info-projeto/info-projeto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-info-projeto',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './info-projeto.component.html',
+  styleUrls: ['./info-projeto.component.css']
+})
+export class InfoProjetoComponent {}
